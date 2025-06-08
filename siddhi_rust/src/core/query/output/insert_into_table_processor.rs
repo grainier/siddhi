35c8e1783dc5cec6d7b4e78f580b5d21b051a025
Grainier Perera <grainier.perera@gmail.com>
2025-06-08 19:56:28 +0530

    Add basic table support

---
+++ siddhi_rust/src/core/query/output/insert_into_table_processor.rs
@@ -0,0 +1,39 @@
+use crate::core::config::siddhi_app_context::SiddhiAppContext;
+use crate::core::config::siddhi_query_context::SiddhiQueryContext;
+use crate::core::event::complex_event::ComplexEvent;
+use crate::core::query::processor::{Processor, CommonProcessorMeta, ProcessingMode};
+use crate::core::table::Table;
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug)]
+pub struct InsertIntoTableProcessor {
+    meta: CommonProcessorMeta,
+    table: Arc<dyn Table>,
+}
+
+impl InsertIntoTableProcessor {
+    pub fn new(table: Arc<dyn Table>, app_ctx: Arc<SiddhiAppContext>, query_ctx: Arc<SiddhiQueryContext>) -> Self {
+        Self { meta: CommonProcessorMeta::new(app_ctx, query_ctx), table }
+    }
+}
+
+impl Processor for InsertIntoTableProcessor {
+    fn process(&self, mut chunk: Option<Box<dyn ComplexEvent>>) {
+        while let Some(mut event) = chunk {
+            let next = event.set_next(None);
+            if let Some(data) = event.get_output_data() {
+                self.table.insert(data);
+            }
+            chunk = next;
+        }
+    }
+
+    fn next_processor(&self) -> Option<Arc<Mutex<dyn Processor>>> { None }
+    fn set_next_processor(&mut self, _next: Option<Arc<Mutex<dyn Processor>>>) {}
+    fn clone_processor(&self, query_ctx: &Arc<SiddhiQueryContext>) -> Box<dyn Processor> {
+        Box::new(Self::new(Arc::clone(&self.table), Arc::clone(&self.meta.siddhi_app_context), Arc::clone(query_ctx)))
+    }
+    fn get_siddhi_app_context(&self) -> Arc<SiddhiAppContext> { Arc::clone(&self.meta.siddhi_app_context) }
+    fn get_processing_mode(&self) -> ProcessingMode { ProcessingMode::DEFAULT }
+    fn is_stateful(&self) -> bool { false }
+}
