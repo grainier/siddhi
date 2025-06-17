61aca51ec3d3c7e01ce71810b4169109a9562156
Grainier Perera <grainier.perera@gmail.com>
2025-06-17 14:04:37 +0530

    Document trigger example and update run script

--- siddhi_rust/examples/run_sample.sh
+++ siddhi_rust/examples/run_sample.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-# Run the example Siddhi app with the CLI
-cargo run --bin run_siddhi examples/sample.siddhi
+# Run a Siddhi app with the CLI. Pass the path to a .siddhi file or run the
+# basic sample if no argument is provided.
+
+APP_PATH=${1:-examples/sample.siddhi}
+cargo run --bin run_siddhi "$APP_PATH"
